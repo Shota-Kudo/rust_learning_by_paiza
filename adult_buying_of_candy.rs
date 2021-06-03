c7fc828e228234821b92c39719a0dda94a78dcc6
Shota-Kudo <64889819+Shota-Kudo@users.noreply.github.com>
2021-06-03 16:24:11 +0900

    Create adult_buying_of_candy.rs

---
+++ adult_buying_of_candy.rs
@@ -0,0 +1,7 @@
+use std::io; 
+fn main(){ 
+    let mut m = String::new(); 
+    io::stdin().read_line(&mut m).ok(); 
+    let m: usize = m.trim().parse().expect("0"); 
+    println!("{}", m/10); 
+}
